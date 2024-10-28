3320f5aa471dfbf411fb4fb1ec70c15678ca3a83
weikaichu1991 <weikai.chu.91@gmail.com>
2024-10-28 16:15:42 +0000

    changing fingle quotation to double quotation, and delete spcaes behind {

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,8 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// const {onRequest} = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
@@ -17,28 +17,28 @@ const logger = require("firebase-functions/logger");
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
-const functions = require('firebase-functions');
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const comments = require('../public/api/comments.route.js');
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const comments = require("../public/api/comments.route.js");
 
 dotenv.config();
 
 const app = express();
-app.use(cors({ origin: true }));
+app.use(cors({origin: true}));
 app.use(express.json());
 
 const dbUsername = process.env.DB_USERNAME;
 const dbPassword = process.env.DB_PASSWORD;
 const mongoURI = `mongodb+srv://${dbUsername}:${dbPassword}@comments.hzn16.mongodb.net/?retryWrites=true&w=majority&appName=comments`;
 
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
+mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log("MongoDB connected"))
     .catch(err => console.log(err));
 
-app.use('/api/v1/comments', comments);
-app.use('*', (req, res) => res.status(404).json({ error: 'not found' }));
+app.use("/api/v1/comments", comments);
+app.use("*", (req, res) => res.status(404).json({error: 'not found'}));
 
 exports.app = functions.https.onRequest(app);
